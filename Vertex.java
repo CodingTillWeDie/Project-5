ebe683b514f5bfbc5833903fcc02bddb430d5b7f
Sean Abi Saab <98626715+seanabisaab@users.noreply.github.com>
2022-04-28 01:03:53 -0700

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -18,8 +20,165 @@ class Vertex<T> implements VertexInterface<T>
         cost = 0;
     } // end constructor
 
-/* Implementations of the vertex operations go here.
-   . . . */
+    /**
+     * Gets this vertex's label.
+     *
+     * @return The object that labels the vertex.
+     */
+    @Override
+    public T getLabel() {
+        return null;
+    }
+
+    /**
+     * Marks this vertex as visited.
+     */
+    @Override
+    public void visit() {
+
+    }
+
+    /**
+     * Removes this vertex's visited mark.
+     */
+    @Override
+    public void unvisit() {
+
+    }
+
+    /**
+     * Sees whether the vertex is marked as visited.
+     *
+     * @return True if the vertex is visited.
+     */
+    @Override
+    public boolean isVisited() {
+        return false;
+    }
+
+    /**
+     * Connects this vertex and a given vertex with a weighted edge.
+     * The two vertices cannot be the same, and must not already
+     * have this edge between them. In a directed graph, the edge
+     * points toward the given vertex.
+     *
+     * @param endVertex  A vertex in the graph that ends the edge.
+     * @param edgeWeight A real-valued edge weight, if any.
+     * @return True if the edge is added, or false if not.
+     */
+    @Override
+    public boolean connect(VertexInterface<T> endVertex, double edgeWeight) {
+        return false;
+    }
+
+    /**
+     * Connects this vertex and a given vertex with an unweighted
+     * edge. The two vertices cannot be the same, and must not
+     * already have this edge between them. In a directed graph,
+     * the edge points toward the given vertex.
+     *
+     * @param endVertex A vertex in the graph that ends the edge.
+     * @return True if the edge is added, or false if not.
+     */
+    @Override
+    public boolean connect(VertexInterface<T> endVertex) {
+        return false;
+    }
+
+    /**
+     * Creates an iterator of this vertex's neighbors by following
+     * all edges that begin at this vertex.
+     *
+     * @return An iterator of the neighboring vertices of this vertex.
+     */
+    @Override
+    public Iterator<VertexInterface<T>> getNeighborIterator() {
+        return null;
+    }
+
+    /**
+     * Creates an iterator of the weights of the edges to this
+     * vertex's neighbors.
+     *
+     * @return An iterator of edge weights for edges to neighbors of this
+     * vertex.
+     */
+    @Override
+    public Iterator<Double> getWeightIterator() {
+        return null;
+    }
+
+    /**
+     * Sees whether this vertex has at least one neighbor.
+     *
+     * @return True if the vertex has a neighbor.
+     */
+    @Override
+    public boolean hasNeighbor() {
+        return false;
+    }
+
+    /**
+     * Gets an unvisited neighbor, if any, of this vertex.
+     *
+     * @return Either a vertex that is an unvisited neighbor or null
+     * if no such neighbor exists.
+     */
+    @Override
+    public VertexInterface<T> getUnvisitedNeighbor() {
+        return null;
+    }
+
+    /**
+     * Records the previous vertex on a path to this vertex.
+     *
+     * @param predecessor The vertex previous to this one along a path.
+     */
+    @Override
+    public void setPredecessor(VertexInterface<T> predecessor) {
+
+    }
+
+    /**
+     * Gets the recorded predecessor of this vertex.
+     *
+     * @return Either this vertex's predecessor or null if no predecessor
+     * was recorded.
+     */
+    @Override
+    public VertexInterface<T> getPredecessor() {
+        return null;
+    }
+
+    /**
+     * Sees whether a predecessor was recorded for this vertex.
+     *
+     * @return True if a predecessor was recorded.
+     */
+    @Override
+    public boolean hasPredecessor() {
+        return false;
+    }
+
+    /**
+     * Records the cost of a path to this vertex.
+     *
+     * @param newCost The cost of the path.
+     */
+    @Override
+    public void setCost(double newCost) {
+
+    }
+
+    /**
+     * Gets the recorded cost of the path to this vertex.
+     *
+     * @return The cost of the path.
+     */
+    @Override
+    public double getCost() {
+        return 0;
+    }
 
     protected class Edge
     {
@@ -49,3 +208,4 @@ class Vertex<T> implements VertexInterface<T>
         } // end getWeight
     } // end Edge
 } // end Vertex
+
