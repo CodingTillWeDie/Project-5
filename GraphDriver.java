a0f4f32ddcddcc79a5c398b80becfe16fe35bca9
Kevin Kim <98494454+kevink0908@users.noreply.github.com>
2022-04-29 10:48:27 -0700

    Update GraphDriver.java

--- GraphDriver.java
+++ GraphDriver.java
@@ -2,125 +2,63 @@ import java.util.Iterator;
 
 public class GraphDriver<T>
 {
-    public static void main(String args[])
+    public void main(String args[])
     {
         // create a graph given in the project prompt.
         Graph<String> graph = new Graph<>(9);
 
+        graph.setLabel(0, "A");
         graph.addEdge(0,1);
         graph.addEdge(0,3);
         graph.addEdge(0,4);
+        graph.setLabel(1, "B");
         graph.addEdge(1,4);
+        graph.setLabel(2, "C");
         graph.addEdge(2,1);
+        graph.setLabel(3, "D");
         graph.addEdge(3,6);
+        graph.setLabel(4, "E");
         graph.addEdge(4,5);
         graph.addEdge(4,7);
+        graph.setLabel(5, "F");
         graph.addEdge(5,2);
         graph.addEdge(5,7);
+        graph.setLabel(6, "G");
         graph.addEdge(6,7);
+        graph.setLabel(7, "H");
         graph.addEdge(7,8);
+        graph.setLabel(8, "I");
         graph.addEdge(8,5);
 
         // perform a breadth-first traversal on the
         // graph starting at vertex A.
-        QueueInterface<Integer> breadthFirst = getBreadthFirstTraversal(0);
+        QueueInterface<Integer> breadthFirst = getBreadthFirstTraversal("A");
 
         // display the vertices in order in which they were visited.
+        System.out.print("Displaying the vertices in breadth-first traversal order: ");
         while (!breadthFirst.isEmpty())
         {
             T label = breadthFirst.dequeue();
 
-            System.out.print(label + “ => ”);
+            System.out.print(label + " -> ");
         }
         System.out.println();
 
 
         // perform a depth-first traversal on the
         // graph starting at vertex A.
-        QueueInterface<Integer> depthFirst = getBreadthFirstTraversal(1);
+        QueueInterface<Integer> depthFirst = getDepthFirstTraversal("A");
 
         // display the vertices in order in which they were visited.
+        System.out.print("Displaying the vertices in depth-first traversal order: ");
         while (!depthFirst.isEmpty())
         {
             T label = depthFirst.dequeue();
 
-            System.out.print(label + “ -> ”);
+            System.out.print(label + " -> ");
         }
         System.out.println();
 
     } // end of main
-    public QueueInterface<T> getBreadthFirstTraversal(T origin)
-    {
-        resetVertices();
-        QueueInterface<T> traversalOrder = new LinkedQueue<>(); // a new queue for the resulting traversal order.
-        QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<>(); // a new queue to hold the vertices as they are visited.
 
-        VertexInterface<T> originVertex = vertices.getValue(origin);
-        originVertex.visit();
-        traversalOrder.enqueue(origin);    // Enqueue vertex label
-        vertexQueue.enqueue(originVertex); // Enqueue vertex
-
-        while (!vertexQueue.isEmpty())
-        {
-            VertexInterface<T> frontVertex = vertexQueue.dequeue();
-            Iterator<VertexInterface<T>> neighbors = frontVertex.getNeighborIterator();
-
-            while (neighbors.hasNext())
-            {
-                VertexInterface<T> nextNeighbor = neighbors.next();
-                if (!nextNeighbor.isVisited())
-                {
-                    nextNeighbor.visit();
-                    traversalOrder.enqueue(nextNeighbor.getLabel());
-                    vertexQueue.enqueue(nextNeighbor);
-                } // end if
-            } // end while
-        } // end while
-
-        return traversalOrder;
-    } // end getBreadthFirstTraversal
-
-    public QueueInterface<T> getDepthFirstTraversal(T origin)
-    {
-        resetVertices();
-        QueueInterface<T> traversalOrder = new LinkedQueue<T>(); // a new queue for the resulting traversal order.
-        StackInterface<VertexInterface<T>> vertexStack = new LinkedStack<>(); // a new stack to hold the vertices as they are visited.
-
-        // mark the origin vertex as visited.
-        VertexInterface<T> originVertex = vertices.getValue(origin);
-        originVertex.visit();
-        traversalOrder.enqueue(origin);
-        vertexStack.push(originVertex);
-
-        while (!vertexStack.isEmpty())
-        {
-            VertexInterface<T> topVertex = vertexStack.peek();
-            VertexInterface<T> nextNeighbor = topVertex.getUnvisitedNeighbor();
-
-            // check to see if top vertex has an unvisited neighbor.
-            if (nextNeighbor != null)
-            {
-                // if so, mark next neighbor as visited.
-                nextNeighbor.visit();
-                traversalOrder.enqueue(nextNeighbor.getLabel());
-                vertexStack.push(nextNeighbor);
-            }
-            // otherwise, all neighbors are visited.
-            else
-                vertexStack.pop();
-        }
-
-        return traversalOrder;
-    }
-    protected void resetVertices()
-    {
-        Iterator<VertexInterface<T>> vertexIterator = vertices.getValueIterator();
-        while (vertexIterator.hasNext())
-        {
-            VertexInterface<T> nextVertex = VertexIterator.next();
-            nextVertex.unvisit();
-            nextVertex.setCost(0);
-            nextVertex.setPredecessor(null);
-        } // end while
-    } // end resetVertices
 } // end of "GraphDriver"
